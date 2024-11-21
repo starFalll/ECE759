c1fd7b0eb05dd586535670510b228ad2c58e9701
starFalll <fyxformal@gmail.com>
2024-11-20 18:13:25 -0600

    add benchmark test scripts

--- source/batchRun.sh
+++ source/batchRun.sh
@@ -1,4 +1,13 @@
-for ((i=1; i <= 8; i++))
+#!/bin/bash
+
+# batch run program from thread 1 to max_thread_num
+
+if [ $# -lt 1 ]; then
+    echo "Usage: $0 <max_thread_num>"
+    exit 1
+fi
+
+for ((i=1; i <= $1; i++))
 do
     # echo "begin run ${i} threads"
     ./stitch_image $i
---
+++ source/benchmark.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# for each schedule type, run from chunk 1,2,4...,16, and save the results
+
+if [ $# -lt 2 ]; then
+    echo "Usage: $0 <schedule_type>(static|dynamic|guided) <max_thread_num>"
+    exit 1
+fi
+
+if [[ "$1" != "static" && "$1" != "dynamic" && "$1" != "guided" ]]; then
+    echo "Error: Invalid schedule type. Must be one of: static, dynamic, guided."
+    exit 1
+fi
+
+cp common.h common_backup.h
+
+for ((i = 1; i <= 16; i *= 2))
+do
+echo """#define FOR_SCHEDULE_TYPE $1
+
+#define CHUNKS_PER_THREAD $i
+
+#define OMP_SCHEDULE(type, chunk) schedule(type, chunk)""" > common.h
+
+sh build.sh
+bash batchRun.sh ${2} | tee ../results/${1}_${i}_threads_8_results.txt
+
+done
+
+cp common_backup.h common.h
+rm common_backup.h
